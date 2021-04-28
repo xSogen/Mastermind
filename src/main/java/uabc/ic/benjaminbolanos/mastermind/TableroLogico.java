e94cae952c06f1b870e969df2a3ab8386a4c6b9d
benjabolanos <bbolanos@uabc.edu.mx>
2021-04-27 19:27:08 -0700

    2021.04.27.02 Se documentaron los metodos

--- src/main/java/uabc/ic/benjaminbolanos/mastermind/TableroLogico.java
+++ src/main/java/uabc/ic/benjaminbolanos/mastermind/TableroLogico.java
@@ -11,11 +11,11 @@ import java.util.Scanner;
  * @author Benjamin Bolaños
  */
 public class TableroLogico {
-    private HashSet<ArrayList> tablero;
-    private ArrayList<Canica> respuesta;
-    private int aciertos[]; //0 para incorrecto, 1 para color correcto, 2 para posicion correcta
-    private final String color[] = {"AZ", "AM", "RO", "NE", "CA", "NA", "MO", "BL"};
-    private int tamañoDeJuego;
+    private HashSet<ArrayList> tablero;     //HashSet de ArrayLists de Canica, lugares del tablero
+    private ArrayList<Canica> respuesta;    //ArrayList de Canica con la combinacion de la respuesta
+    private int aciertos[];                 //0 para incorrecto, 1 para color correcto, 2 para posicion correcta
+    private final String color[] = {"AZ", "AM", "RO", "NE", "CA", "NA", "MO", "BL"};    //Colores posibles
+    private int tamañoDeJuego;              //Entero para saber cuantas canica habra
 
 
     public TableroLogico(int tamañoDeJuego, int oportunidades) {
@@ -49,32 +49,65 @@ public class TableroLogico {
         this.aciertos = aciertos;
     }
 
+    /**
+     * Metodo que crea 'tamañoDeJuego' canicas y las mete en el ArrayList respuesta.
+     * Las canicas pueden ser duplicadas.
+     */
     public void crearRespuesta(){
         for (int i = 0; i<tamañoDeJuego; i++){
+            //Añade una Canica que crea a partir de un color random.
             respuesta.add(new Canica(colorRandom(),1));
         }
     }
+    
+    /**
+     * Metodo que crea 'tamañoDeJuego' canicas y las mete en el ArrayList respuesta.
+     * Las canicas no pueden ser duplicadas.
+     */
     public void crearRespuestaSD(){
+        //El ciclo corre mientras 'respuesta' no este lleno.
         while (respuesta.size()<tamañoDeJuego){
-            String color = colorRandom();
-            Canica canica = new Canica(color,1);
-            boolean duplicado = false;
+            String color = colorRandom();               //Crea un color random
+            Canica canica = new Canica(color,1);        //Se crea una canica con el color random
+            boolean duplicado = false;                  //Booleano que inicia en falso, determinara si la canica es duplicada
+            
+            /*
+                Ciclo foreach para cada canica dentro de respuesta.
+                Para cada canica de respuesta se comparara la nueva
+                canica creada. Si se encuentra que hay dos iguales y se marcara
+                como duplicada.
+            */
             for(Canica can : respuesta){
                 if(canica.getColor() == can.getColor())
                     duplicado = true;
             }
+            
+            //Si la canica no es marcada como duplicada, se añadira al ArrayList de respuesta
             if(!duplicado) respuesta.add(canica);
         }
     }
 
+    /**
+     * Metodo para ingresar una combinacion y comprobar que no sea repetida
+     * @return True si la combinacion se añadio con exito y no es repetida
+     */
     public boolean ingresarCombinacion(){
         Scanner sc = new Scanner(System.in);
-        ArrayList<Canica> combinacion = new ArrayList();
+        ArrayList<Canica> combinacion = new ArrayList();        //ArrayList de la combinacion del usuario
+        
+        /*
+            Ciclo que corre 'tamañoDeJuego' veces, para cada iteracion 
+            creara una canica dependiendo del codigo ingresado. Las canicas
+            creadas se añaden a combinacion.
+        */
         for(int i = 0; i < tamañoDeJuego; i++){
             System.out.print("Introduzca la combinacion: ");
             combinacion.add(new Canica(sc.nextLine(),1));
         }
-        boolean exito=false;
+        
+        boolean exito=false;                        //Booleano que nos dice si hubo exito al añadir la combinacion a tablero
+        
+        //Si la combinacion no es repetida, esta se añade al tablero y exito es true.
         if(!comprobarCombinacion(combinacion)){
             tablero.add(combinacion);
             exito=true;
@@ -84,26 +117,29 @@ public class TableroLogico {
 
     /**
      * Metodo que recibe un ArrayList y lo compara con los demas ArrayList de tablero.
-     * @param al Recibe un ArrayList de Canica que es la combinacion ingresada por el usuario
+     * @param combinacion Recibe un ArrayList de Canica que es la combinacion ingresada por el usuario
      * @return Retorna true si el ArrayList es duplicado.
      */
-    public boolean comprobarCombinacion(ArrayList<Canica> al){
+    public boolean comprobarCombinacion(ArrayList<Canica> combinacion){
         boolean duplicado=false;
         int cont;
-        for (ArrayList<Canica> tab : tablero) {
+        for (ArrayList<Canica> combinacionesTablero : tablero) {
             cont = 0;
             for (int i = 0; i < tamañoDeJuego; i++) {
-                Canica can1 = al.get(i);
-                Canica can2 = tab.get(i);
+                Canica can1 = combinacion.get(i);
+                Canica can2 = combinacionesTablero.get(i);
                 if (can1.getColor().equals(can2.getColor()))
                     cont++;
             }
             if (cont == tamañoDeJuego) duplicado = true;
         }
         return duplicado;
-
     }
 
+    /**
+     * Metodo que retorna un color de una posicion random de los colores posibles
+     * @return String con el color de una posicion aleatoria
+     */
     public String colorRandom(){
         Random random = new Random();
         int num = random.nextInt(8);

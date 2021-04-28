2a9f9dfaf13e12a80fc318c94f507efc2c797444
benjabolanos <bbolanos@uabc.edu.mx>
2021-04-27 19:34:28 -0700

    2021.04.27.03 Se añadieron clases para mastermind grafico

---
+++ src/main/java/uabc/ic/benjaminbolanos/mastermind/Rectangulo.java
@@ -0,0 +1,71 @@
+package uabc.ic.benjaminbolanos.mastermind;
+
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Benjamin Bolaños
+ * @version 2021.04.26
+ */
+public class Rectangulo {
+    private int base;
+    private int altura;
+    private int xPosition;
+    private int yPosition;
+    private String color;
+    private boolean isVisible;
+    private Canvas canvas;
+    
+    public Rectangulo(int base, int altura, int xPosition, int yPosition, String color, Canvas canvas){
+        this.base=base;
+        this.color=color;
+        this.canvas=canvas;
+        this.xPosition=xPosition;
+        this.yPosition=yPosition;
+        this.altura=altura;
+    }
+    
+    public void makeVisible()
+    {
+        isVisible = true;
+        draw();
+    }
+    
+    public void makeInvisible()
+    {
+        erase();
+        isVisible = false;
+    }
+    
+    public void changeColor(String newColor)
+    {
+        color = newColor;
+        draw();
+    }
+    
+    private void draw()
+    {
+        if(isVisible) {
+            canvas.draw(this, color,
+                        new Rectangle(xPosition, yPosition, base,altura));
+            canvas.wait(10);
+        }
+    }
+    
+    private void erase()
+    {
+        if(isVisible) {
+            canvas.erase(this);
+        }
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setIsVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+    
+    
+}
